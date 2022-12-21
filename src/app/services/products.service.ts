250928d162474086073edbcbc3f2412e7a8d3ec3
Sergii Kostanets <sergiikostanets@gmail.com>
2022-12-21 13:52:44 +0000

    error handler don't work

--- src/app/services/products.service.ts
+++ src/app/services/products.service.ts
@@ -1,3 +1,4 @@
+import { ErrorService } from './error.service';
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpErrorResponse, HttpParams } from '@angular/common/http'
 import { catchError, delay, Observable, throwError } from 'rxjs';
@@ -8,22 +9,25 @@ import { IProduct } from '../models/product';
 })
 
 export class ProductService {
-  constructor(private http: HttpClient) {
+  constructor(
+    private http: HttpClient,
+    private errorService: ErrorService
+  ) {
   }
 
   getAll(): Observable<IProduct[]> {
     return this.http.get<IProduct[]>('https://fakestoreapi.com/products1', {
       params: new HttpParams({
-        fromObject: {limit: 2}
+        fromObject: {limit: 5}
       })
     }).pipe(
       delay(2000),
-      catchError(this.errorHandler)
+      catchError(this.errorHandler.bind(this))
     )
   }
 
   private errorHandler(error: HttpErrorResponse) {
-
+    this.errorService.handle(error.message)
     return throwError(() => error.message)
   }
 }
