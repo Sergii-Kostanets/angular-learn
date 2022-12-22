e1ad0441f9ec711b87cdf0f4d66a2fbdc8239cca
Sergii Kostanets <skostanets@chill.ie>
2022-12-22 18:11:08 +0000

    continue creating adding new product

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,23 +14,22 @@ export class AppComponent implements OnInit {
   title = 'Angular Learniang app'
   // products: IProduct[] = []
   loading = false
-  products$: Observable<IProduct[]>
+  // products$: Observable<IProduct[]>
   term = ''
 
   constructor(
-    private productsService: ProductService,
+    public productsService: ProductService,
     public modalService: ModalService
   ) {
   }
 
   ngOnInit(): void {
     this.loading = true
-    // this.productsService.getAll().subscribe(products => {
-    //   this.products = products
-    //   this.loading = false
-    // })
-    this.products$ = this.productsService.getAll().pipe(
-      tap(() => this.loading = false)
-    )
+    this.productsService.getAll().subscribe(() => {
+      this.loading = false
+    })
+    // this.products$ = this.productsService.getAll().pipe(
+    //   tap(() => this.loading = false)
+    // )
   }
 }
