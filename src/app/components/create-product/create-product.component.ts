8372894af2f14337fb98fb23394967af2ac3e597
Sergii Kostanets <skostanets@chill.ie>
2022-12-22 18:03:16 +0000

    sent to server

--- src/app/components/create-product/create-product.component.ts
+++ src/app/components/create-product/create-product.component.ts
@@ -1,5 +1,7 @@
+import { ProductService } from './../../services/products.service';
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { ModalService } from 'src/app/services/modal.service';
 
 @Component({
   selector: 'app-create-product',
@@ -7,6 +9,12 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
   styleUrls: ['./create-product.component.scss']
 })
 export class CreateProductComponent implements OnInit {
+
+  constructor(
+    private productService: ProductService,
+    private modalService: ModalService
+  ) {
+  }
 
   form = new FormGroup({
     title: new FormControl<string>('', [
@@ -19,13 +27,24 @@ export class CreateProductComponent implements OnInit {
     return this.form.controls.title as FormControl
   }
 
-  constructor() { }
-
   ngOnInit(): void {}
 
   submit() {
     // console.log(this.title)
-    console.log(this.form.value)
+    // console.log(this.form.value)
+    this.productService.create({
+      title: this.form.value as string,
+      price: 13.5,
+      description: 'lorem ipsum set',
+      image: 'https://i.pravatar.cc',
+      category: 'electronic',
+      rating: {
+        rate: 42,
+        count: 1,
+      }
+    }).subscribe(() => {
+      this.modalService.close()
+    })
   }
 
 }
